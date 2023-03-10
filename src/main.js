cf3ac0f6e4359cbdfa2953dcda32ddf9bec0e0fa
Valentin Dassonville <valentin.dassonville2.etu@univ-lille.fr>
2023-03-10 11:30:18 +0100

    rond au milieu de l'écran

--- src/main.js
+++ src/main.js
@@ -1,36 +1,20 @@
 const canvas = document.querySelector('.gameCanvas'),
 	context = canvas.getContext('2d');
 
-/*function resampleCanvas() {
-	canvas.width = canvas.clientWidth;
-	canvas.height = canvas.clientHeight;
-}
-
-const canvasResizeObserver = new ResizeObserver(() => resampleCanvas());
-canvasResizeObserver.observe(canvas);*/
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
 let x = 1000,
 	y = 400;
-
-const image = new Image();
-image.src = '/images/logo.png';
-
-image.addEventListener('load', event => {
-	context.drawImage(image, 50, 50);
-});
 
 render();
 
 function render() {
-	//context.clearRect(0, 0, canvas.width, canvas.height);
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	context.clearRect(0, 0, canvas.width, canvas.height);
 	context.beginPath();
-	context.moveTo(x, y);
 	context.strokeStyle = 'blue';
 	context.fillStyle = 'blue';
-	context.arc(x, y, 10, 0, 2 * Math.PI, false);
+	context.arc(canvas.width / 2, canvas.height / 2, 10, 0, 2 * Math.PI, false);
 	context.fill();
 	context.stroke();
+	requestAnimationFrame(render);
 }
