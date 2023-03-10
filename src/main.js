cc4f2d5b067c8aecd7410588bd4392a70c4b1cb3
Valentin Dassonville <valentin.dassonville2.etu@univ-lille.fr>
2023-03-10 10:33:40 +0100

    canvas + rond

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-console.log('coucou');
+const canvas = document.querySelector('.gameCanvas'),
+	context = canvas.getContext('2d');
+
+/*function resampleCanvas() {
+	canvas.width = canvas.clientWidth;
+	canvas.height = canvas.clientHeight;
+}
+
+const canvasResizeObserver = new ResizeObserver(() => resampleCanvas());
+canvasResizeObserver.observe(canvas);*/
+
+console.log(canvas.clientWidth);
+console.log(canvas.width);
+
+let x = 50,
+	y = 50;
+
+render();
+
+function render() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.beginPath();
+	context.moveTo(x, y);
+	context.strokeStyle = 'blue';
+	context.fillStyle = 'blue';
+	context.arc(x, y, 10, 0, 2 * Math.PI, false);
+	context.fill();
+	context.stroke();
+}
