00a001374d816660ca1f22b4f9201348d9030bc6
Valentin Dassonville <valentin.dassonville2.etu@univ-lille.fr>
2023-03-12 01:32:43 +0100

    déplacement du joueur

---
+++ src/Vec2D.js
@@ -0,0 +1,47 @@
+export default class Vec2D {
+
+    x;
+    y;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(other) {
+        this.x += other.x;
+        this.y += other.y;
+    }
+
+    substract(other) {
+        this.x -= other.x;
+        this.y -= other.y;
+    }
+
+    multiply(n) {
+        this.x *= n;
+        this.y *= n;
+    }
+
+    length() {
+        return Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y, 2));
+    }
+
+    normalize() {
+        const len = this.length();
+        if(len != 0) {
+            this.x /= len;
+            this.y /= len;
+        }
+    }
+
+    setLength(len) {
+        this.normalize();
+        this.multiply(len);
+    }
+
+    distance(other) {
+        return Math.sqrt(Math.pow(this.x-other.x, 2) + Math.pow(this.y-other.y, 2));
+    }
+
+}
--- src/main.js
+++ src/main.js
@@ -1,29 +1,63 @@
+import Player from "./Player.js";
+import Vec2D from "./Vec2D.js";
+
 const canvas = document.querySelector('.gameCanvas'),
 	context = canvas.getContext('2d');
 
-let x = 1000,
-	y = 400;
+const player = new Player(50, 50);
 
-let score = 10;
+const foods = [];
+
+setup();
+
+function setup() {
+	for(let i=0; i<50; i++) {
+		foods.push({
+			x: Math.floor(Math.random()*1000-500),
+			y: Math.floor(Math.random()*1000-500)
+		});
+	}
+}
+
+const dir = new Vec2D(0, 0);
+
+canvas.addEventListener('mousemove', event => {
+	dir.x = event.clientX-canvas.width/2;
+	dir.y = event.clientY-canvas.height/2;
+});
+
+setInterval(() => {
+	player.update(dir);
+}, 1000/60);
+
 render();
-
 function render() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.translate(canvas.width/2, canvas.height/2);
+	context.translate(-player.pos.x, -player.pos.y);
+
+	foods.forEach(food => {
+		context.beginPath();
+		context.arc(food.x, food.y, 3, 0, 2*Math.PI, false);
+		context.fill();
+		context.stroke();
+	});
+
 	context.beginPath();
 	context.strokeStyle = 'blue';
 	context.fillStyle = 'blue';
 	context.arc(
-		canvas.width / 2,
-		canvas.height / 2,
-		score,
+		player.pos.x,
+		player.pos.y,
+		player.score,
 		0,
 		2 * Math.PI,
 		false
 	);
 	context.fill();
 	context.stroke();
-	//context.translate(0, 100);
+
 	requestAnimationFrame(render);
 }
