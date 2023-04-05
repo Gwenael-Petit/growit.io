88ec4f65b0c9cd8a60f2d9275ba6aa1c07013e04
Valentin Dassonville <valentin.dassonville2.etu@univ-lille.fr>
2023-04-05 09:32:35 +0200

    remet la config client en js

--- webpack.config.js
+++ webpack.config.js
@@ -15,21 +15,26 @@ export default {
 	module: {
 		rules: [
 			{
-				test: /\.(ts|js)$/, // tous les fichiers js ou ts ...
+				test: /\.js$/, // tous les fichiers js ...
+				//test: /\.(ts|js)$/, // tous les fichiers js ou ts ...
 				exclude: /node_modules/, // ... sauf le dossier node_modules ...
 				use: {
+					// ... seront compilés par babel !
+					loader: 'babel-loader',
+
 					// ... seront compilés par tsc !
-					loader: 'ts-loader',
+					/*loader: 'ts-loader',
 					options: {
 						configFile: 'tsconfig.client.json',
-					},
+					},*/
 				},
+				type: 'javascript/esm', // permet l'utilisation des modules ES6
 			},
 		],
 	},
-	resolve: {
+	/*resolve: {
 		extensions: ['.ts', '.js'],
-	},
+	},*/
 	devtool: 'source-map',
 	devServer: {
 		hot: false, // désactivation hot-reload (inutilisé)
